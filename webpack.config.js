8d2b299e56b71889f511ba3a6652c64d0599d1a0
Celine <celine.pro.morel@gmail.com>
2024-06-19 16:20:04 +0200

    ajout user entity, ajout bootstrap, ajout webpack encore, ajout des bundles nécessaires

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    .addEntry('app', './assets/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    })
+    .enableSassLoader()
+    // .enableTypeScriptLoader()
+    .enableIntegrityHashes(Encore.isProduction())
+    .autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
